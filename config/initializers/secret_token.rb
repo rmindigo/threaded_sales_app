ac7d4236b20775ec52561b30e75083f928dbf276
Ryan <ryan.mindigo@gmail.com>
2015-07-09 09:06:23 -0700

    generated the app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ThreadedSalesApp::Application.config.secret_key_base = 'bcb4d2c40bab3bf220125cd4790b767a9feac58781af5841c83a42e3168b3b1ed792dab6c2510972c1533f79ac0311373a6fecc43d682c852229969c1420a034'
