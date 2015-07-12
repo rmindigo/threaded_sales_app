8eb444c44f1273764a432c97b5fdd4c5dc76fc25
Ryan <ryan.mindigo@gmail.com>
2015-07-11 21:52:17 -0700

    added devise and name to users table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150710183822) do
+ActiveRecord::Schema.define(version: 20150712043326) do
 
   create_table "post_hierarchies", id: false, force: true do |t|
     t.integer "ancestor_id",   null: false
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 20150710183822) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
