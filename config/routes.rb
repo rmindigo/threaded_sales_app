1afc76c6559a679e47e8e0452babbd08fa9a47a5
Ryan <ryan.mindigo@gmail.com>
2015-07-20 16:37:53 -0700

    added events pages

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ ThreadedSalesApp::Application.routes.draw do
   devise_for :users
   
   resources :users, only: [:show, :index]
+  resources :static_pages, only: [:index]
   resources :posts, only: [:index, :show, :create]
   get '/posts/new/(:parent_id)', to: 'posts#new', as: :new_post
 
